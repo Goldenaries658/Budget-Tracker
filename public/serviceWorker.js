1eaa274d7810389798d2f974b1e9da1cae2d4a43
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-08-08 17:43:20 +0100

    Improved caching to automatically cache responses

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,5 +1,5 @@
 // Perform install steps
-const CACHE_NAME = "my-site-cache-v3";
+const CACHE_NAME = "budget-app-cache-v2";
 const urlsToCache = [
   "/",
   "/styles/main.css",
@@ -8,25 +8,22 @@ const urlsToCache = [
   "images/icons/icon-144x144.png",
 ];
 
+const cache = (request, response) => {
+  if (response.type === "error" || response.type === "opaque") {
+    return Promise.resolve(); // do not put in cache network errors
+  }
+
+  return caches
+    .open(CACHE_NAME)
+    .then((cache) => cache.put(request, response.clone()));
+};
+
 self.addEventListener("install", (event) => {
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       console.log("Opened cache");
       return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-// Returning cached requests
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
     })
   );
 });
@@ -46,4 +43,28 @@ self.addEventListener("activate", (event) => {
       );
     })
   );
+});
+
+// Returning cached requests
+self.addEventListener("fetch", (event) => {
+  // Checking if calling api
+  // if (event.request.url.includes("/api/")) {
+  //   // Cache Update Refresh Strategy for api calls
+  // } else {
+  // Cache First Strategy
+  console.log(event.request.url);
+  event.respondWith(
+    caches
+      .match(event.request) // Checking for cached response
+      .then((cachedFile) => cachedFile || fetch(event.request)) // if cachedFile unavailable then req network
+      .then((response) => {
+        if (event.request.url.indexOf("http") === 0) {
+          // Checking if request is http (not browser extension)
+          return cache(event.request, response) // adds response to cache
+            .then(() => response); // Return network response
+        }
+        return response;
+      })
+  );
+  // }
 });
